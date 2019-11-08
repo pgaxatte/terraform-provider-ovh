73276e865470ed4583d5c2830ad6cf04a189d9a8
Pierre Gaxatte <pierre.gaxatte@corp.ovh.com>
2019-11-08 09:57:58 +0100

    WIP acceptance tests

---
+++ ovh/data_source_ovh_me_ssh_keys_test.go
@@ -0,0 +1,27 @@
+package ovh
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccMeSshKeysDataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMeSshKeysDatasourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(
+						"data.ovh_me_ssh_keys.keys", "names"),
+				),
+			},
+		},
+	})
+}
+
+const testAccMeSshKeysDatasourceConfig = `
+data "ovh_me_ssh_keys" "keys" {}
+`
